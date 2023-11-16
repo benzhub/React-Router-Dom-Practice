def495be12d6081bd3e8d6864b4ed58066cbce0c
lenrich <lenrich1403@gmail.com>
2023-11-17 07:11:29 +0800

    7. Fetching Data With React Router Loaders Pizza Menu

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
-import Menu from "./features/menu/Menu";
+import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
 import CreateOrder from "./features/order/CreateOrder";
@@ -8,15 +8,17 @@ import AppLayout from "./ui/AppLayout";
 
 const router = createBrowserRouter([
   {
-    element: <AppLayout/>,
+    element: <AppLayout />,
     children: [
       { path: "/", element: <Home /> },
-      { path: "/menu", element: <Menu /> },
+      // 我們在載入/menu的時候，我們就同時fetching data
+      // 以前在組件中Fetching data的作法是必須先Render後才fetchting data
+      { path: "/menu", element: <Menu />, loader: menuLoader },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
-    ]
-  }
+    ],
+  },
 ]);
 
 export default function App() {
--- src/features/menu/MenuItem.jsx
+++ src/features/menu/MenuItem.jsx
@@ -1,4 +1,8 @@
+import { formatCurrency } from "../../utils/helpers";
+
+// eslint-disable-next-line react/prop-types
 function MenuItem({ pizza }) {
+  // eslint-disable-next-line react/prop-types, no-unused-vars
   const { id, name, unitPrice, ingredients, soldOut, imageUrl } = pizza;
 
   return (
@@ -6,6 +10,7 @@ function MenuItem({ pizza }) {
       <img src={imageUrl} alt={name} />
       <div>
         <p>{name}</p>
+        {/* eslint-disable-next-line react/prop-types */}
         <p>{ingredients.join(', ')}</p>
         <div>
           {!soldOut ? <p>{formatCurrency(unitPrice)}</p> : <p>Sold out</p>}
