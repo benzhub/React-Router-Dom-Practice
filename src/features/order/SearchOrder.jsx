64556114439623ec3ea6a84cf9435a8e87cd4b90
lenrich <lenrich1403@gmail.com>
2023-11-17 09:36:54 +0800

    10. Fetching Orders

---
+++ src/features/order/SearchOrder.jsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function SearchOrder() {
+    const navigate = useNavigate();
+  const [query, setQuery] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    if(!query) return;
+    navigate(`/order/${query}`);
+    setQuery("");
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        placeholder="Seaerh order #"
+        value={query}
+        onChange={(e) => {
+          setQuery(e.target.value);
+        }}
+      />
+    </form>
+  );
+}
