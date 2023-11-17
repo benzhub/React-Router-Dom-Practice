3decbb35bd4026d97d555932019778f6b68995de
lenrich <lenrich1403@gmail.com>
2023-11-17 09:19:10 +0800

    9. Handling Errors With Error Elements

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Home from "./ui/Home";
+import Error from "./ui/Error";
 import Menu, { loader as menuLoader } from "./features/menu/Menu";
 import Cart from "./features/cart/Cart";
 import Order from "./features/order/Order";
@@ -13,7 +14,12 @@ const router = createBrowserRouter([
       { path: "/", element: <Home /> },
       // 我們在載入/menu的時候，我們就同時fetching data
       // 以前在組件中Fetching data的作法是必須先Render後才fetchting data
-      { path: "/menu", element: <Menu />, loader: menuLoader },
+      {
+        path: "/menu",
+        element: <Menu />,
+        loader: menuLoader,
+        errorElement: <Error />,
+      },
       { path: "/cart", element: <Cart /> },
       { path: "/order/new", element: <CreateOrder /> },
       { path: "/order/:orderId", element: <Order /> },
